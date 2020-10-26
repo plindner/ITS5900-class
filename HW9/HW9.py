8e8419fce30844135b95b658f430b5b27af43bec
dynamicprosperity <73482238+dynamicprosperity@users.noreply.github.com>
2020-10-26 13:16:54 -0400

    Update HW9.py

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,18 +1,14 @@
-#Importing a module
+
 import os
 
-#Defining a function with the file path, and an indent 
+ 
 def process_dir(p, indent):
-    #For loop in a file path you are directing
-    for item in os.listdir(path):
-#If statement to indent a file 
+    for item in os.listdir(path): 
         if os.path.isfile("{0}/{1}".format(path, item)):
             print("\t"*indent, item)
         elif os.path.isdir("{0}/{1}".format(path, item)):
-#Color code the output
             print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-#Infinite loop for indenting files; commented out because it is not needed
-#            process_dir("{0}/{1}".format(path, item), indent+1)
+            process_dir("{0}/{1}".format(path, item), indent+1)
         else:
             print("Not a file or directory")
 
