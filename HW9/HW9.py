0644893b23a872b557923e51d39058834fa368a6
Justin Zeyeum <justinzeyeum@yahoo.co.uk>
2020-10-27 09:30:42 -0400

    Error Handling

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -3,22 +3,28 @@ import os
 
 # Function with two arguments
 def process_dir(p, indent):
-    # Looping through the given file path  
-    for item in os.listdir(path): 
-        # Condition for checking the file
-        if os.path.isfile("{0}/{1}".format(path, item)): 
-            # printing out the indented format of the indent from file call item
-            print("\t"*indent, item)  
-        # Condition for checking the file path
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            # Using ANSI color code to color the output
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            # Processing the directory incrementally, causing the program to run continously,
-            # Hence the code is commented
-            #process_dir("{0}/{1}".format(path, item), indent+1)
-        # Else condition for wrong file directory
-        else:
-            print("Not a file or directory")
+    try:
+
+        # Looping through the given file path  
+        for item in os.listdir(path): 
+            # Condition for checking the file
+            if os.path.isfile("{0}/{1}".format(path, item)): 
+                # printing out the indented format of the indent from file call item
+                print("\t"*indent, item)  
+            # Condition for checking the file path
+            elif os.path.isdir("{0}/{1}".format(path, item)):
+                # Using ANSI color code to color the output
+                print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
+                # Processing the directory incrementally, causing the program to run continously,
+                # Hence the code is commented
+                #process_dir("{0}/{1}".format(path, item), indent+1)
+            # Else condition for wrong file directory
+            else:
+                print("Not a file or directory")
+    # Error handling
+    except:
+        print("Please enter valid path")
+
 # Prompting user for input file path
 path=input("Enter a valid system path: ")
 print("\x1b[1;31m{}\x1b[0;0m".format(path))
