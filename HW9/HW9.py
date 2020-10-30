673ba4a26f2355b934281478b7975009aebf56f6
db012920 <73483487+db012920@users.noreply.github.com>
2020-10-30 14:25:49 -0400

    Update HW9.py
    try and exception added and bug fixed

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,17 +1,28 @@
-
+# importing os module
 import os
 
- 
+
+# defining a function with two arguments
 def process_dir(p, indent):
-    for item in os.listdir(path): 
-        if os.path.isfile("{0}/{1}".format(path, item)):
-            print("\t"*indent, item)
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            process_dir("{0}/{1}".format(path, item), indent+1)
-        else:
-            print("Not a file or directory")
+    try:
+    #loop through a file path
+        for item in os.listdir(path):
+            # identifying a file
+            if os.path.isfile("{0}/{1}".format(path, item)):
+                print("\t"*indent, item)
+            # identifying a directory as alternate loop
+            elif os.path.isdir("{0}/{1}".format(path, item)):
+                # indent and apply color to output
+                print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
+                # this code is redundant so i commented it out
+                '''process_dir("{0}/{1}".format(path, item), indent+1)'''
+                # else statement when a path is not a directory
+            else:
+                print("Not a file or directory")
+    except:
+        print("your input does not exist")
 
 path=input("Enter a valid system path: ")
+#print files in color
 print("\x1b[1;31m{}\x1b[0;0m".format(path))
 process_dir(path, 1)
