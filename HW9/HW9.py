9b0f32348e60ab939cce8ecf1c3a35a6f42ed194
Esther Agyekum <EGH00323@SURFLINEGH.LOCAL>
2020-10-31 21:01:11 +0000

    Requirement 4 - error detection

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,6 +1,7 @@
 import os #import os module
 
 def process_dir(p, indent): #function that processes input path and output content folders and files
+    try:
         for item in os.listdir(p): #loop through the items in the directory
             if os.path.isfile("{0}/{1}".format(p, item)): #condition to check if current item is an existing file
                 print("\t"*indent, item) #print item if it is a file and uses number tabs depending on indent number
@@ -10,6 +11,8 @@ def process_dir(p, indent): #function that processes input path and output conte
                 process_dir("{0}/{1}".format(p, item), indent+1) 
             else:
                 print("Not a file or directory") #print when input is not file or directory
+    except Exception: #catch errors of invalid/inaccessible path or directory
+        print('Error! File/Directory cannot be found') #print this error message to user
 
 path=input("Enter a valid system path: ") #accepting path input from user and assigning to a variable
 print("\x1b[1;31m{}\x1b[0;0m".format(path)) #print path/directory inputed in red color
