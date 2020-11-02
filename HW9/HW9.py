1f71e92a9aae4b0b6db66214fe64771b8570fdb9
coalwells96 <70669801+coalwells96@users.noreply.github.com>
2020-11-01 22:54:54 -0500

    Update HW9.py

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,17 +1,32 @@
 
 import os
 
- 
-def process_dir(p, indent):
-    for item in os.listdir(path): 
-        if os.path.isfile("{0}/{1}".format(path, item)):
-            print("\t"*indent, item)
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            process_dir("{0}/{1}".format(path, item), indent+1)
-        else:
-            print("Not a file or directory")
+# creating a function that reads the contents of a given filepath
+# with different behavior if it is a file or directory
+# it indents and prints the content of the file/directory
+# it will continue expanding the contents of a directory if it finds
+# another
 
+# the {0}/{1}s exist as placeholders to be replaced by the format's
+# arguments
+
+# "\x1b[1;31m{}\x1b[0;0m" is just formatting used to define color in the output.
+# Directories and file paths show up in red, files in white.
+
+
+def process_dir(p, indent):
+    try:
+        for item in os.listdir(p): 
+            if os.path.isfile("{0}\{1}".format(p, item)):
+                print("\t"*indent, item)
+            elif os.path.isdir("{0}\{1}".format(p, item)):
+                print("\t"*(indent+1), "\x1b[1;31m{}\x1b[0;0m".format(item))
+                process_dir("{0}\{1}".format(p, item), indent+1)
+            else:
+                print("Not a file or directory")
+                return
+    except:
+        print("Invalid input.")
 path=input("Enter a valid system path: ")
 print("\x1b[1;31m{}\x1b[0;0m".format(path))
 process_dir(path, 1)
