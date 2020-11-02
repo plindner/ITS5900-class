b10530814b0912e4d518ecbcad5d8f1f6998d775
Omoniyi Olufemi <olufemi.o.david@gmail.com>
2020-11-02 11:51:29 +0100

    Includes an error detection mechanism

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -5,15 +5,18 @@ import os
 #function definition to process a directory and list its content 
 def process_dir(p, indent):
     for item in os.listdir(path):
-        #Run if "item" is a file and print it out with some indentation 
-        if os.path.isfile("{0}/{1}".format(path, item)):
-            print("\t"*indent, item)
-        #Run if "item" is a directory and print it out with some indentation
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            #process_dir("{0}/{1}".format(path, item), indent+1) commented out recursion error
-        else:
-            print("Not a file or directory")
+        try:
+            #Run if "item" is a file and print it out with some indentation 
+            if os.path.isfile("{0}/{1}".format(path, item)):
+                print("\t"*indent, item)
+            #Run if "item" is a directory and print it out with some indentation
+            elif os.path.isdir("{0}/{1}".format(path, item)):
+                print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
+                #process_dir("{0}/{1}".format(path, item), indent+1) commented out recursion error
+            else:
+                print("Not a file or directory")
+        except RecursionError as error:
+            pass
 
 path=input("Enter a valid system path: ")
 print("\x1b[1;31m{}\x1b[0;0m".format(path))
