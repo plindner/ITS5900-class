6630e221b4d04a24ddc76ad9ac6d19ce37b4a30e
Augustinekludze <73483463+Augustinekludze@users.noreply.github.com>
2020-11-07 17:26:58 +0000

    added an error detection code

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -3,14 +3,16 @@ import os
 
  
 def process_dir(path, indent):
-    for item in os.listdir(path): #loop through the items in the directory
-        if os.path.isfile("{0}/{1}".format(path, item)): #check if item is a file
-            print("\t"*indent, item) #print the item in the directory 
-        elif os.path.isdir("{0}/{1}".format(path, item)):#check if item is a subfolder
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item)) #print the subfolder
-            process_dir("{0}/{1}".format(path, item), indent+1) #call the method to process the path of the directory
-        else:
-            print("Not a file or directory") #print it's not a file or directory
+    try :
+        for item in os.listdir(path): #loop through the items in the directory
+        
+            if os.path.isfile("{0}/{1}".format(path, item)): #check if item is a file
+                print("\t"*indent, item) #print the item in the directory 
+            if os.path.isdir("{0}/{1}".format(path, item)):#check if item is a subfolder
+                print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item)) #print the subfolder
+                process_dir("{0}/{1}".format(path, item), indent+1) #call the method to process the path of the directory   
+    except Exception:
+             print("Not a file or directory") #print it's not a file or directory
 
 path=input("Enter a valid system path: ") #allowing user to print path
 print("\x1b[1;31m{}\x1b[0;0m".format(path)) #print path in red 
