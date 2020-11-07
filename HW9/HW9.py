d43c99c8064129e14c3e5b591320843efe0272f0
Augustinekludze <73483463+Augustinekludze@users.noreply.github.com>
2020-11-07 17:19:28 +0000

    fixed the errors in the code and added comments

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -2,16 +2,16 @@
 import os
 
  
-def process_dir(p, indent):
-    for item in os.listdir(path): 
-        if os.path.isfile("{0}/{1}".format(path, item)):
-            print("\t"*indent, item)
-        elif os.path.isdir("{0}/{1}".format(path, item)):
-            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item))
-            process_dir("{0}/{1}".format(path, item), indent+1)
+def process_dir(path, indent):
+    for item in os.listdir(path): #loop through the items in the directory
+        if os.path.isfile("{0}/{1}".format(path, item)): #check if item is a file
+            print("\t"*indent, item) #print the item in the directory 
+        elif os.path.isdir("{0}/{1}".format(path, item)):#check if item is a subfolder
+            print("\t"*indent, "\x1b[1;31m{}\x1b[0;0m".format(item)) #print the subfolder
+            process_dir("{0}/{1}".format(path, item), indent+1) #call the method to process the path of the directory
         else:
-            print("Not a file or directory")
+            print("Not a file or directory") #print it's not a file or directory
 
-path=input("Enter a valid system path: ")
-print("\x1b[1;31m{}\x1b[0;0m".format(path))
-process_dir(path, 1)
+path=input("Enter a valid system path: ") #allowing user to print path
+print("\x1b[1;31m{}\x1b[0;0m".format(path)) #print path in red 
+process_dir(path, 1) #call the method process directory
